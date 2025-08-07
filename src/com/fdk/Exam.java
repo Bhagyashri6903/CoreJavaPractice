bbcffe16b24fa071dc335cd000b7ccdf4285f81a
Bhagyashri Sunil Patil <bhagyashripatil434@gmail.com>
2025-08-07 10:29:02 +0530

    Add files via upload

---
+++ src/com/fdk/Exam.java
@@ -0,0 +1,9 @@
+package com.fdk;
+public class Exam {
+    public static void main(String[] args) {
+        
+        int result = Calculator.add(5, 10);
+        
+        System.out.println("The sum is: " + result); 
+    }
+}
