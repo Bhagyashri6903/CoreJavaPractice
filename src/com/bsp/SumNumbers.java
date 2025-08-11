85fab0488aa4aa86f7e00709e8b6799919152997
Bhagyashri Sunil Patil <bhagyashripatil434@gmail.com>
2025-08-11 19:38:54 +0530

    Add files via upload

---
+++ src/com/bsp/SumNumbers.java
@@ -0,0 +1,14 @@
+package com.bsp;
+
+public class SumNumbers {
+
+	public static void main(String[] args) {
+		int[] arr = {2,4,5,7,8,3};
+		int sum = 0;
+		for (int i=0; i<arr.length;i++) {
+			sum += arr[i];
+		}
+		System.out.println("Sum of array is:" + sum);
+	}
+
+}
