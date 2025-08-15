a99bc677abd7409561eeeaa9465facf04c34ce4a
Bhagyashri Sunil Patil <bhagyashripatil434@gmail.com>
2025-08-15 20:02:58 +0530

    Add files via upload

---
+++ src/com/fdk/A.java
@@ -0,0 +1,11 @@
+package com.fdk;
+
+public class A {
+	
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.m11();
+		s.m12();
+	}
+
+}
---
+++ src/com/fdk/Student.java
@@ -0,0 +1,18 @@
+package com.fdk;
+
+public class Student {
+	
+	public int Sid;
+	protected String Sname;
+	
+	public void m11()
+	{
+		System.out.println(Sid);
+	}
+	
+	protected void m12() {
+		System.out.println(Sname);
+	
+	}
+
+}
